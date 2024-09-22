8ec2875df4a02f4b06291c733eb6a20bccabb7f4
FergusCurrie <fergus.currie@gmail.com>
2024-09-22 10:39:13 +1000

    dump

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -29,7 +29,7 @@ them with correct access token. localStorage holds this token?
 */
 api.interceptors.request.use(
   (config) => {
-    //const token = localStorage.getItem(ACCESS_TOKEN);
+    //const token = localStorage.getItem(ACCESS_TOKEN);x
     // if (token) {
     //   config.headers.Authorization = `Bearer ${token}`;
     // }
